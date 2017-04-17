9a5ab820f911f0d71aaeb4c024fea1dd62edd1d1
LinhBM <linhbm@outlook.com>
2017-04-17 15:31:49 +0700

    Filling in layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   scope "(:locale)", locale:/en|vn/ do
 
-<<<<<<< HEAD
   root "static_pages#home" 
 
   get  "static_pages/home"
@@ -12,19 +11,7 @@ Rails.application.routes.draw do
   get  "static_pages/about"
 
   get  "static_pages/contact"
-=======
-  root 'static_pages#home' 
 
-  get  'static_pages/home'
-
-  get  'static_pages/help'
-
-  get  'static_pages/about'
-
-  get  'static_pages/contact'
->>>>>>> b94cabd... Rails flavored ruby
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
 end
 end
