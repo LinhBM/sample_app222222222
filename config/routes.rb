e1f085c6214b9f6111297ef1a581437638968524
LinhBM <linhbm@outlook.com>
2017-04-17 15:31:49 +0700

    Rails flavored ruby

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   scope "(:locale)", locale:/en|vn/ do
 
+<<<<<<< HEAD
   root "static_pages#home" 
 
   get  "static_pages/home"
@@ -11,7 +12,19 @@ Rails.application.routes.draw do
   get  "static_pages/about"
 
   get  "static_pages/contact"
+=======
+  root 'static_pages#home' 
+
+  get  'static_pages/home'
+
+  get  'static_pages/help'
+
+  get  'static_pages/about'
+
+  get  'static_pages/contact'
+>>>>>>> b94cabd... Rails flavored ruby
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
 end
